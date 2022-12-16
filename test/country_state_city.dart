2663595826308300c466ebb64f40160bdcf4305c
paulpascal <paul.alogno@gmail.com>
2022-12-16 12:22:05 +0000

    fix: get all countries issue & update doc

--- test/country_state_city.dart
+++ test/country_state_city.dart
@@ -3,12 +3,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:country_state_city/country_state_city.dart';
 
 void main() {
-  test('test if files are loaded', () async {
-    final countries = await getAllCountries();
-    final states = await getAllStates();
-    final cities = await getAllCities();
-    expect(countries.isEmpty, false);
-    expect(states.isEmpty, false);
-    expect(cities.isEmpty, false);
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Country_State_City', () {
+    test('coutries should be loaded', () async {
+      final countries = await getAllCountries();
+
+      expect(countries.isEmpty, false);
+      expect(countries.first.runtimeType, Country);
+    });
+
+    test('states should be loaded', () async {
+      final states = await getAllStates();
+
+      expect(states.isEmpty, false);
+      expect(states.first.runtimeType, State);
+    });
+
+    test('cities should be loaded', () async {
+      final cities = await getAllCities();
+
+      expect(cities.isEmpty, false);
+      expect(cities.first.runtimeType, City);
+    });
   });
 }
